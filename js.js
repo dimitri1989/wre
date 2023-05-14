318ab741bf7b36acf4bfcf2ff11511872767710f
dimitri1989 <dimitri.kvarelashvili@gmail.com>
2023-05-14 23:33:42 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,6 @@
+function dadastureba(){
+    document.getElementById("blues").style.backgroundColor="green";
+    document.getElementById("blues").style.position = "absolute";
+    document.getElementById("blues").style.right="0px";
+
+}
